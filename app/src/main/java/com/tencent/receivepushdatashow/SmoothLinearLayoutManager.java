8a0874a75a49653000c926448f42cdfeb40aefb4
XiFanYin <807142217@qq.com>
2020-08-17 18:18:45 +0800

    解决bug

--- app/src/main/java/com/tencent/receivepushdatashow/SmoothLinearLayoutManager.java
+++ app/src/main/java/com/tencent/receivepushdatashow/SmoothLinearLayoutManager.java
@@ -96,7 +96,7 @@ public class SmoothLinearLayoutManager extends LinearLayoutManager implements Re
 
     @Override
     public void onChildViewAttachedToWindow(@NonNull View view) {
-        if (mOnViewPagerListener != null) {
+        if (mOnViewPagerListener != null && getChildCount() == 1) {
             mOnViewPagerListener.onInitComplete(view);
         }
     }
